1bdded680bd962fc258dc04a2053178ccfbb6f77
willwelch2000 <willwelch2000@gmail.com>
2024-11-21 23:17:54 -0600

    updates to port mapping

--- RunVHDLSharp/Program.cs
+++ RunVHDLSharp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VHDLSharp;
+
+Module module1 = new()
+{
+    Name = "m1",
+};
+Module module2 = new()
+{
+    Name = "m2",
+};
+PortMapping portMapping = new(module1, module2);
+Signal s1 = new("s1", module1);
+Signal s2 = new("s2", module2);
+Port p1 = new()
+{
+    Signal = s1, 
+    Direction = PortDirection.Input,
+};
+module1.Ports.Add(p1);
+KeyValuePair<Port, Signal> kvp = new(p1, s2);
+portMapping.Add(kvp);
+portMapping.Remove(kvp);
+var a = 5;
--- VHDLSharp/src/PortMapping.cs
+++ VHDLSharp/src/PortMapping.cs
@@ -1,15 +1,56 @@
+using System.Collections;
+
 namespace VHDLSharp;
 
 /// <summary>
-/// Mapping of a port to 
+/// Exception related to mapping ports
+/// </summary>
+public class PortMappingException : Exception
+{
+    /// <summary>
+    /// Parameterless constructor
+    /// </summary>
+    public PortMappingException() : base("A custom exception has occurred.")
+    {
+    }
+
+    /// <summary>
+    /// Constructor that accepts a custom message
+    /// </summary>
+    /// <param name="message"></param>
+    public PortMappingException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Constructor that accepts a custom message and inner exception
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public PortMappingException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
+
+/// <summary>
+/// Mapping of ports of a module to the signals its connected to in an instantiation
 /// </summary>
 public class PortMapping : IDictionary<Port, Signal>
 {
-    private Module module;
+    private readonly Module module;
 
-    private Module parent;
+    private readonly Module parent;
     
-    private Dictionary<Port, Signal> backendDictionary;
+    private readonly Dictionary<Port, Signal> backendDictionary = [];
+
+    private ICollection<KeyValuePair<Port, Signal>> BackendDictionaryAsCollection
+    {
+        get
+        {
+            ICollection<KeyValuePair<Port, Signal>> collection = backendDictionary;
+            return collection;
+        }
+    }
 
     /// <summary>
     /// Construct port mapping given instantiated module and parent module
@@ -21,7 +62,34 @@ public class PortMapping : IDictionary<Port, Signal>
         this.module = module;
         this.module.ModuleUpdated += ModuleUpdated;
         this.parent = parent;
-        backendDictionary = [];
+    }
+
+    /// <summary>
+    /// Get all ports that need assignment
+    /// </summary>
+    public IEnumerable<Port> PortsToAssign => module.Ports.Except(Keys);
+
+    /// <inheritdoc/>
+    public ICollection<Port> Keys => backendDictionary.Keys;
+
+    /// <inheritdoc/>
+    public ICollection<Signal> Values => backendDictionary.Values;
+
+    /// <inheritdoc/>
+    public int Count => backendDictionary.Count;
+
+    /// <inheritdoc/>
+    public bool IsReadOnly => false;
+
+    /// <summary>
+    /// Indexer for port mapping
+    /// </summary>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    public Signal this[Port port]
+    {
+        get => backendDictionary[port];
+        set => Add(port, value);
     }
 
     private void ModuleUpdated(object? sender, EventArgs eventArgs)
@@ -34,13 +102,57 @@ public class PortMapping : IDictionary<Port, Signal>
         foreach ((Port port, Signal signal) in backendDictionary)
         {
             if (port.Signal.Parent != module)
-                throw new Exception($"Ports must have the specified module {module} as parent");
+                throw new PortMappingException($"Ports must have the specified module {module} as parent");
             if (!module.Ports.Contains(port))
-                throw new Exception($"Port {port} must be in the list of ports of specified module {module}");
+                throw new PortMappingException($"Port {port} must be in the list of ports of specified module {module}");
             if (signal.Parent != parent)
-                throw new Exception($"Signal must have module {parent} as parent");
+                throw new PortMappingException($"Signal must have module {parent} as parent");
         }
     }
 
-    // Add stuff to automatically check validity
+    /// <summary>
+    /// True if port mapping is complete (all ports are assigned)
+    /// </summary>
+    /// <returns></returns>
+    public bool Complete() => module.Ports.All(backendDictionary.ContainsKey);
+
+    /// <inheritdoc/>
+    public void Add(Port port, Signal signal)
+    {
+        backendDictionary.Add(port, signal);
+        CheckValid();
+    }
+
+    /// <inheritdoc/>
+    public void Add(KeyValuePair<Port, Signal> item) => Add(item.Key, item.Value);
+
+    /// <inheritdoc/>
+    public bool ContainsKey(Port port) => backendDictionary.ContainsKey(port);
+
+    /// <inheritdoc/>
+    public bool Remove(Port port) => backendDictionary.Remove(port);
+
+    /// <inheritdoc/>
+    public bool TryGetValue(Port port, out Signal signal) => backendDictionary.TryGetValue(port, out signal);
+
+    /// <inheritdoc/>
+    public void Clear() => backendDictionary.Clear();
+
+    /// <inheritdoc/>
+    public bool Contains(KeyValuePair<Port, Signal> item) => backendDictionary.Contains(item);
+
+    /// <inheritdoc/>
+    public void CopyTo(KeyValuePair<Port, Signal>[] array, int arrayIndex) => BackendDictionaryAsCollection.CopyTo(array, arrayIndex);
+
+    /// <inheritdoc/>
+    public bool Remove(KeyValuePair<Port, Signal> item) => BackendDictionaryAsCollection.Remove(item);
+
+    /// <inheritdoc/>
+    public IEnumerator<KeyValuePair<Port, Signal>> GetEnumerator() => backendDictionary.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        IEnumerable enumerable = backendDictionary;
+        return enumerable.GetEnumerator();
+    }
 }
