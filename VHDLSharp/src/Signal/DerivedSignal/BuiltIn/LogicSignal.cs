3eb9322bf858b6579b0d56a14eb53ebf4a409fbe
willwelch2000 <100434115+willwelch2000@users.noreply.github.com>
2025-11-01 13:27:30 -0500

    logic signal and simulation fix

---
+++ VHDLSharp/src/Signal/DerivedSignal/BuiltIn/LogicSignal.cs
@@ -0,0 +1,145 @@
+using VHDLSharp.Behaviors;
+using VHDLSharp.Dimensions;
+using VHDLSharp.LogicTree;
+using VHDLSharp.Modules;
+
+namespace VHDLSharp.Signals.Derived;
+
+/// <summary>
+/// A signal that comes from a <see cref="LogicExpression"/>
+/// </summary>
+public class LogicSignal : DerivedSignal
+{
+    /// <summary>
+    /// Constructor given an expression and parent module
+    /// </summary>
+    /// <param name="expression">Expression defining this signal</param>
+    /// <param name="parentModule">The module to which this signal belongs. If null, it is extracted from the expression</param>
+    /// <exception cref="Exception">
+    /// If no parent module is provided and it can't be extracted from the expression,
+    /// or if it is provided but doesn't match the expression's module
+    /// </exception>
+    public LogicSignal(LogicExpression expression, IModule? parentModule = null) : base(parentModule ??
+        expression.BaseObjects.OfType<IModuleSpecificSignal>().FirstOrDefault()?.ParentModule ??
+        throw new Exception("If no parent module is provided, the provided expression must contain a module-specific signal so that a module can be found"))
+    {
+        // Exception if expression module doesn't match provided module
+        if (parentModule is not null && expression.BaseObjects.OfType<IModuleSpecificSignal>().FirstOrDefault() is IModuleSpecificSignal moduleSpecificSignal
+            && !moduleSpecificSignal.ParentModule.Equals(parentModule))
+            throw new Exception($"Module of expression ({moduleSpecificSignal.ParentModule}) must equal provided base module ({parentModule})");
+
+        Expression = expression;
+    }
+
+    /// <summary>Expression defining this signal</summary>
+    public LogicExpression Expression { get; }
+
+    /// <inheritdoc/>
+    public override DefiniteDimension Dimension => Expression.Dimension;
+
+    /// <inheritdoc/>
+    protected override IEnumerable<IModuleSpecificSignal> InputSignalsWithAssignedModule => UnpackDerivedSignals(Expression.BaseObjects.OfType<IModuleSpecificSignal>());
+
+    /// <inheritdoc/>
+    protected override IInstantiation CompileWithoutCheck(string moduleName, string instanceName)
+    {
+        Module childModule = new(moduleName);
+        Dictionary<INamedSignal, ITopLevelNamedSignal> signalMappings = [];
+        TransformExpressionOutput output = Expression.GenerateLogicalObject(TransformExpressionOptions, new() { NewParentModule = childModule, SignalMappings = signalMappings });
+        int dimension = Expression.Dimension.NonNullValue;
+        ITopLevelNamedSignal outputSignal = dimension switch
+        {
+            1 => new Signal("Output", childModule),
+            > 1 => new Vector("Output", childModule, dimension),
+            _ => throw new Exception("Dimension should not be < 1"),
+        };
+        childModule.SignalBehaviors[outputSignal] = new LogicBehavior(output.Expression);
+
+        // Handle ports and port mapping
+        Instantiation instantiation = new(childModule, ParentModule, instanceName);
+        Port outputPort = childModule.AddNewPort(outputSignal, PortDirection.Output);
+        instantiation.PortMapping[outputPort] = ((IDerivedSignal)this).GetLinkedSignal();
+        foreach ((INamedSignal topLevelSignal, ITopLevelNamedSignal childSignal) in signalMappings)
+        {
+            Port inputPort = childModule.AddNewPort(childSignal, PortDirection.Input);
+            instantiation.PortMapping[inputPort] = topLevelSignal;
+        }
+
+        return instantiation;
+    }
+
+    // Takes a logic expression and recreates it with signals in another module
+    // Derived signals use their linked signals instead
+    // The input dictionary is mutated and shared between all calls of the functions
+    private CustomLogicObjectOptions<ISignal, TransformExpressionInput, TransformExpressionOutput>? transformExpressionOptions = null;
+    private CustomLogicObjectOptions<ISignal, TransformExpressionInput, TransformExpressionOutput> TransformExpressionOptions
+    {
+        get
+        {
+            if (transformExpressionOptions is not null)
+                return transformExpressionOptions;
+
+            CustomLogicObjectOptions<ISignal, TransformExpressionInput, TransformExpressionOutput> options = new();
+
+            options.AndFunction = TransformExpressionAndFunction;
+            options.OrFunction = TransformExpressionOrFunction;
+            options.NotFunction = TransformExpressionNotFunction;
+            options.BaseFunction = TransformExpressionBaseFunction;
+
+            transformExpressionOptions = options;
+            return options;
+        }
+    }
+
+    private TransformExpressionOutput TransformExpressionBaseFunction(ISignal signal, TransformExpressionInput additionalInput)
+    {
+        IModule newParentModule = additionalInput.NewParentModule;
+        Dictionary<INamedSignal, ITopLevelNamedSignal> signalMappings = additionalInput.SignalMappings;
+        // If this is a module-specific signal, get the linked signal directly or indirectly
+        if (signal is IModuleSpecificSignal)
+        {
+            INamedSignal namedSignal = signal switch
+            {
+                INamedSignal expAsNamedSignal => expAsNamedSignal,
+                IDerivedSignal { LinkedSignal: INamedSignal linkedSignal } => linkedSignal,
+                IDerivedSignalNode { LinkedSignal: INamedSignal linkedSignal } => linkedSignal,
+                _ => throw new Exception("Could not get signal as linked signal--it might be a derived signal without a linked signal"),
+            };
+            // If already mapped, use that one
+            if (signalMappings.TryGetValue(namedSignal, out ITopLevelNamedSignal? mapping))
+                return new() { Expression = mapping };
+            string name = $"Input{signalMappings.Count}";
+            int dimension = namedSignal.Dimension.NonNullValue;
+            ITopLevelNamedSignal newMapping = dimension switch
+            {
+                1 => new Signal(name, newParentModule),
+                > 1 => new Vector(name, newParentModule, dimension),
+                _ => throw new Exception("Dimension should not be < 1"),
+            };
+            signalMappings[namedSignal] = newMapping;
+            return new() { Expression = newMapping };
+        }
+        if (signal is ISignalWithKnownValue)
+            return new() { Expression = signal };
+        throw new Exception("Signal is neither of type IModuleSpecificSignal or ISignalWithKnownValue");
+    }
+
+    private TransformExpressionOutput TransformExpressionNotFunction(ILogicallyCombinable<ISignal> expression, TransformExpressionInput additionalInput) =>
+        new() { Expression = expression.GenerateLogicalObject(TransformExpressionOptions, additionalInput).Expression };
+
+    private TransformExpressionOutput TransformExpressionAndFunction(IEnumerable<ILogicallyCombinable<ISignal>> innerExpressions, TransformExpressionInput additionalInput) => new() { Expression = new And<ISignal>([.. innerExpressions.Select(exp => exp.GenerateLogicalObject(TransformExpressionOptions, additionalInput).Expression)]) };
+
+    private TransformExpressionOutput TransformExpressionOrFunction(IEnumerable<ILogicallyCombinable<ISignal>> innerExpressions, TransformExpressionInput additionalInput) => new() { Expression = new Or<ISignal>([.. innerExpressions.Select(exp => exp.GenerateLogicalObject(TransformExpressionOptions, additionalInput).Expression)]) };
+
+    private class TransformExpressionInput
+    {
+        public required IModule NewParentModule { get; set; }
+
+        public required Dictionary<INamedSignal, ITopLevelNamedSignal> SignalMappings { get; set; }
+    }
+
+    private class TransformExpressionOutput
+    {
+        public ILogicallyCombinable<ISignal> Expression { get; set; } = null!;
+    }
+}
---
+++ VHDLSharpTests/Signal/Derived/LogicSignalTests.cs
@@ -0,0 +1,224 @@
+using VHDLSharp.Modules;
+using VHDLSharp.Signals;
+using VHDLSharp.Signals.Derived;
+using VHDLSharp.Simulations;
+using VHDLSharp.Validation;
+
+namespace VHDLSharpTests;
+
+[TestClass]
+public class LogicSignalTests
+{
+    private const double timeBuffer = 5e-7;
+
+    [TestMethod]
+    // Uses a LogicSignal and then uses that in an AddedSignal
+    public void TestNestedSignal()
+    {
+        ValidityManager.GlobalSettings.MonitorMode = MonitorMode.Inactive;
+        Module module = new("mod1");
+        Vector s1 = module.GenerateVector("s1", 2);
+        Vector s2 = module.GenerateVector("s2", 2);
+        Vector s3 = module.GenerateVector("s3", 2);
+        Vector s4 = module.GenerateVector("s4", 2);
+        s4.AssignBehavior(s1.And(s2).ToSignal().Plus(s3));
+        Port s1p = module.AddNewPort(s1, PortDirection.Input);
+        Port s2p = module.AddNewPort(s2, PortDirection.Input);
+        Port s3p = module.AddNewPort(s3, PortDirection.Input);
+        module.AddNewPort(s4, PortDirection.Output);
+
+        string vhdl = module.GetVhdl();
+        string expectedVhdl =
+        """
+        library ieee
+        use ieee.std_logic_1164.all;
+
+        entity mod1 is
+            port (
+                s1	: in	std_logic_vector(1 downto 0);
+                s2	: in	std_logic_vector(1 downto 0);
+                s3	: in	std_logic_vector(1 downto 0);
+                s4	: out	std_logic_vector(1 downto 0)
+            );
+        end mod1;
+
+        architecture rtl of mod1 is
+            signal DerivedSignal0	: std_logic_vector(1 downto 0)
+            signal DerivedSignal1	: std_logic_vector(1 downto 0)
+
+            component Adder_2bits_noCIn_noCOut
+                port (
+                    A	: in	std_logic_vector(1 downto 0);
+                    B	: in	std_logic_vector(1 downto 0);
+                    Y	: out	std_logic_vector(1 downto 0)
+                );
+            end component Adder_2bits_noCIn_noCOut;
+            
+            component DerivedModule1
+                port (
+                    Output	: out	std_logic_vector(1 downto 0);
+                    Input0	: in	std_logic_vector(1 downto 0);
+                    Input1	: in	std_logic_vector(1 downto 0)
+                );
+            end component DerivedModule1;
+            
+        begin
+            DerivedInstance0 : Adder_2bits_noCIn_noCOut
+                port map (
+                    A => DerivedSignal1,
+                    B => s3,
+                    Y => DerivedSignal0
+                );
+            
+            DerivedInstance1 : DerivedModule1
+                port map (
+                    Output => DerivedSignal1,
+                    Input0 => s1,
+                    Input1 => s2
+                );
+            
+            s4 <= DerivedSignal0;
+        end rtl;
+
+        entity Adder_2bits_noCIn_noCOut is
+            port (
+                A	: in	std_logic_vector(1 downto 0);
+                B	: in	std_logic_vector(1 downto 0);
+                Y	: out	std_logic_vector(1 downto 0)
+            );
+        end Adder_2bits_noCIn_noCOut;
+
+        architecture rtl of Adder_2bits_noCIn_noCOut is
+            signal COut0	: std_logic
+
+            component Adder_1bit_noCIn
+                port (
+                    A	: in	std_logic;
+                    B	: in	std_logic;
+                    Y	: out	std_logic;
+                    COut	: out	std_logic
+                );
+            end component Adder_1bit_noCIn;
+            
+            component Adder_1bit_noCOut
+                port (
+                    A	: in	std_logic;
+                    B	: in	std_logic;
+                    Y	: out	std_logic;
+                    CIn	: in	std_logic
+                );
+            end component Adder_1bit_noCOut;
+            
+        begin
+            Adder0 : Adder_1bit_noCIn
+                port map (
+                    A => A[0],
+                    B => B[0],
+                    Y => Y[0],
+                    COut => COut0
+                );
+            
+            Adder1 : Adder_1bit_noCOut
+                port map (
+                    A => A[1],
+                    B => B[1],
+                    Y => Y[1],
+                    CIn => COut0
+                );
+            
+        end rtl;
+
+        entity Adder_1bit_noCIn is
+            port (
+                A	: in	std_logic;
+                B	: in	std_logic;
+                Y	: out	std_logic;
+                COut	: out	std_logic
+            );
+        end Adder_1bit_noCIn;
+
+        architecture rtl of Adder_1bit_noCIn is
+        begin
+            Y <= ((A or B) and (not ((A and B))));
+            COut <= (A and B);
+        end rtl;
+
+        entity Adder_1bit_noCOut is
+            port (
+                A	: in	std_logic;
+                B	: in	std_logic;
+                Y	: out	std_logic;
+                CIn	: in	std_logic
+            );
+        end Adder_1bit_noCOut;
+
+        architecture rtl of Adder_1bit_noCOut is
+            signal aXorB	: std_logic
+        begin
+            aXorB <= ((A or B) and (not ((A and B))));
+            Y <= ((aXorB or CIn) and (not ((aXorB and CIn))));
+        end rtl;
+
+        entity DerivedModule1 is
+            port (
+                Output	: out	std_logic_vector(1 downto 0);
+                Input0	: in	std_logic_vector(1 downto 0);
+                Input1	: in	std_logic_vector(1 downto 0)
+            );
+        end DerivedModule1;
+
+        architecture rtl of DerivedModule1 is
+        begin
+            Output <= (Input0 and Input1);
+        end rtl;
+        """;
+        Assert.IsTrue(Util.AreEqualIgnoringWhitespace(expectedVhdl, vhdl));
+
+        // Run simulation
+        RuleBasedSimulation simulation = new(module, new DefaultTimeStepGenerator() { MaxTimeStep = 1e-6 })
+        {
+            Length = 64e-5
+        };
+        simulation.StimulusMapping[s1p] = new MultiDimensionalStimulus([
+            new PulseStimulus(1e-5, 1e-5, 2e-5),
+            new PulseStimulus(2e-5, 2e-5, 4e-5),
+        ]);
+        simulation.StimulusMapping[s2p] = new MultiDimensionalStimulus([
+            new PulseStimulus(4e-5, 4e-5, 8e-5),
+            new PulseStimulus(8e-5, 8e-5, 16e-5),
+        ]);
+        simulation.StimulusMapping[s3p] = new MultiDimensionalStimulus([
+            new PulseStimulus(16e-5, 16e-5, 32e-5),
+            new PulseStimulus(32e-5, 32e-5, 64e-5),
+        ]);
+
+        SubcircuitReference moduleRef = new(module, []);
+        simulation.SignalsToMonitor.Add(moduleRef.GetChildSignalReference(s1));
+        simulation.SignalsToMonitor.Add(moduleRef.GetChildSignalReference(s2));
+        simulation.SignalsToMonitor.Add(moduleRef.GetChildSignalReference(s3));
+        simulation.SignalsToMonitor.Add(moduleRef.GetChildSignalReference(s4));
+        ISimulationResult[] results = [.. simulation.Simulate()];
+        ISimulationResult s1Results = results[0];
+        ISimulationResult s2Results = results[1];
+        ISimulationResult s3Results = results[2];
+        ISimulationResult s4Results = results[3];
+
+        // Check results
+        for (int i = 0; i < s1Results.TimeSteps.Length; i++)
+        {
+            double timeStep = s1Results.TimeSteps[i];
+            // Checks if we're within timeBuffer of a transition point
+            if (Math.Abs(timeStep - 1e-5 * Math.Round(timeStep / 1e-5, 0)) < timeBuffer)
+                continue;
+
+            int i1 = s1Results.Values[i];
+            int i2 = s2Results.Values[i];
+            int i3 = s3Results.Values[i];
+            int i4 = s4Results.Values[i];
+
+            int sum = (s1Results.Values[i] & s2Results.Values[i]) + s3Results.Values[i];
+            int expectedS4 = sum % 4;
+            Assert.AreEqual(expectedS4, s4Results.Values[i]);
+        }
+    }
+}
